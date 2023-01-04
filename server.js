e4a524b36e7c088a02fd46b97e71dee2f70ada01
Chris <kjman26@gmail.com>
2023-01-04 12:13:07 -0800

    added inquirer prompts

--- server.js
+++ server.js
@@ -12,3 +12,60 @@ const connection = mysql.createConnection({
     database: 'employeeDB' 
 });
 
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log('connected as id' + connection.threadId);
+    console.log('Welcome the Official Employee Manager')
+    
+    //run app
+    initialPrompt();
+});
+
+function initialPrompt(){
+    //inquirer prompt
+    inquirer
+        .createPromptModule({
+            type: 'list',
+            name: 'task',
+            message: 'What would you like to do?',
+            choices: [
+                "See Employees",
+                "See Employees by Department",
+                "Add New Employee",
+                "Remove Employee(s)",
+                "Update Employee Info",
+                "Add Role",
+                "Finish"]
+        })
+        .then(function ({ task }) {
+            switch (task) {
+                case "See Employees":
+                    seeEmployees();
+                    break;
+                
+                case "See Employees by Department":
+                    seeEmployeesByDepartment();
+                    break;
+                
+                case "Add New Employee":
+                    addNewEmployee();
+                    break;
+
+                case "Remove Employee(s)":
+                    removeEmployees();
+                    break;
+
+                case "Update Employee Info":
+                    updateEmployeeInfo();
+                    break;
+
+                case "Add Role":
+                    addRole();
+                    break
+
+                case "Finish":
+                    connection.end();
+                    break;
+            }
+        });
+}
