926f2e15fd11661a79aea539eb9edbd235f27008
Chris <kjman26@gmail.com>
2023-01-04 21:00:41 -0800

    added in roles

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,5 +1,7 @@
 USE employeeDB;
 
+-- departments
+
 INSERT INTO department (name)
 VALUES ("Human Resources");
 INSERT INTO department (name)
@@ -7,5 +9,18 @@ VALUES ("Software Engineering");
 INSERT INTO department (name)
 VALUES ("Marketing");
 INSERT INTO department (name)
-VALUES ("Legal");
+VALUES ("Sales");
 
+-- insert roles
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Recruiter", 150000, 1);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Lead Engineer", 200000, 2);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Associate Software Engineer", 120000, 2);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Outside Sales Specialist", 125000, 3);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Manager", 175000, 4);
+
+-- insert employees
