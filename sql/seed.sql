4c727fabbaf5c5338468c1f7d54991c81ee94244
Chris <kjman26@gmail.com>
2023-01-04 20:55:05 -0800

    added departments

---
+++ sql/seed.sql
@@ -0,0 +1,11 @@
+USE employeeDB;
+
+INSERT INTO department (name)
+VALUES ("Human Resources");
+INSERT INTO department (name)
+VALUES ("Software Engineering");
+INSERT INTO department (name)
+VALUES ("Marketing");
+INSERT INTO department (name)
+VALUES ("Legal");
+
